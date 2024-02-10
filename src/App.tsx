6acd4643b986babca4c5ee9cb038ee6526194a31
Travis Davisson <travis@acorns.com>
2024-02-10 08:56:41 -0800

    Revert "Blank data branch"

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import row8 from "./data/row8.json";
 import row9 from "./data/row9.json";
 import row10 from "./data/row10.json";
 import topRow from "./data/topRow.json";
-import React from "react";
 
 function App() {
   return (
@@ -19,9 +18,9 @@ function App() {
         Rotate device or widen your browser window
       </p>
       <p className="rotateMessage">Good Luck!</p>
-      <div className="team1">Team 1</div>
+      <div className="niners">San Francisco</div>
       <div className="boardContainer">
-        <div className="team2">Team 2</div>
+        <div className="chiefs">Kansas City</div>
         <div className="topRow">
           {topRow.map((box, i) => (
             <div key={`top row column ${i}`}>{box?.text}</div>
